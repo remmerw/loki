95dede56f14e6b0a972437ce96b899af603672ca
remmerw <remmer.wilts@gmail.com>
2025-06-25 13:33:29 +0200

    Version 0.2.2 ------------- - Kotlin 2.2.0

--- loki/build.gradle.kts
+++ loki/build.gradle.kts
@@ -1,6 +1,5 @@
 @file:OptIn(ExperimentalWasmDsl::class)
 
-import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
@@ -91,7 +90,7 @@ android {
 
 
 mavenPublishing {
-    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    publishToMavenCentral()
 
     signAllPublications()
 
