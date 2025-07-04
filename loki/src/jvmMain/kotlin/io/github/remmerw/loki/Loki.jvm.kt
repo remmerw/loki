249f69b4a638e287b9247e3dfb8e8983d0857875
remmerw <remmer.wilts@gmail.com>
2025-07-04 18:15:42 +0200

    Version 0.2.7 ------------- - Memory improvements

--- loki/src/jvmMain/kotlin/io/github/remmerw/loki/Loki.jvm.kt
+++ loki/src/jvmMain/kotlin/io/github/remmerw/loki/Loki.jvm.kt
@@ -1,8 +1,14 @@
 package io.github.remmerw.loki
+import io.github.remmerw.loki.mdht.hostname
 import io.ktor.network.sockets.InetSocketAddress
 import java.net.InetAddress
 
 actual fun createInetSocketAddress(address: ByteArray, port:Int) : InetSocketAddress {
-    val inet = InetAddress.getByAddress(address)
-    return InetSocketAddress(inet.hostName, port)
+    if(address.size == 16) {
+        val inet = InetAddress.getByAddress(address)
+        return InetSocketAddress(inet.hostName, port)
+    } else {
+        val host = hostname(address)
+        return InetSocketAddress(host, port)
+    }
 }
