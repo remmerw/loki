1f9b866346747836a937027f0efa9c6a453f9960
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-17 15:36:27 +0545

    Aktualisieren von Store.kt

--- loki/src/commonMain/kotlin/io/github/remmerw/loki/core/Store.kt
+++ loki/src/commonMain/kotlin/io/github/remmerw/loki/core/Store.kt
@@ -2,12 +2,12 @@ package io.github.remmerw.loki.core
 
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
-import java.net.InetSocketAddress
+import io.github.remmerw.nott.Address
 
 interface Store {
-    suspend fun addresses(limit: Int): List<InetSocketAddress>
+    suspend fun addresses(limit: Int): List<Address>
 
-    suspend fun store(address: InetSocketAddress)
+    suspend fun store(address: Address)
 }
 
 @Suppress("unused")
@@ -15,15 +15,15 @@ class MemoryStore(
     private val maxSize: Int = 10000,
 ) : Store {
     private val mutex = Mutex()
-    private val peers: MutableSet<InetSocketAddress> = LinkedHashSet(maxSize)
+    private val peers: MutableSet<Address> = LinkedHashSet(maxSize)
 
-    override suspend fun addresses(limit: Int): List<InetSocketAddress> {
+    override suspend fun addresses(limit: Int): List<Address> {
         mutex.withLock {
             return peers.take(limit).toList()
         }
     }
 
-    override suspend fun store(address: InetSocketAddress) {
+    override suspend fun store(address: Address) {
         mutex.withLock {
             if (peers.size >= maxSize) {
                 // FIFO removal
