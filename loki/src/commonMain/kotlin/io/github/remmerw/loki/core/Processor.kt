f63b2bc9669766636217177e2620de78e38197eb
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-17 16:44:17 +0545

    Aktualisieren von Processor.kt

--- loki/src/commonMain/kotlin/io/github/remmerw/loki/core/Processor.kt
+++ loki/src/commonMain/kotlin/io/github/remmerw/loki/core/Processor.kt
@@ -74,7 +74,7 @@ internal fun CoroutineScope.performConnection(
                     try {
                         Socket().use { socket ->
                             socket.soTimeout = 10000
-                            socket.connect(address, 3000)
+                            socket.connect(address.inetAddress(), address.port.toUnt(), 3000)
 
                             Connection(
                                 address,
