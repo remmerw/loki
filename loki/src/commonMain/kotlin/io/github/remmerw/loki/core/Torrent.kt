007f24765d0d494db5ad3a46b82e597e50a7564e
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-22 16:30:59 +0545

    Aktualisieren von Torrent.kt

--- loki/src/commonMain/kotlin/io/github/remmerw/loki/core/Torrent.kt
+++ loki/src/commonMain/kotlin/io/github/remmerw/loki/core/Torrent.kt
@@ -132,6 +132,7 @@ private fun buildHashes(hashes: ByteArray): List<ByteArray> {
 }
 
 internal fun buildTorrent(metadata: ByteBuffer): Torrent {
+    metadata.flip()
     require(metadata.hasRemaining()) { "Can't parse bytes array: empty" }
     val reader = BEReader(metadata)
 
