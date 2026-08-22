b7fa7b9f00377fe7880063c84fc31888bc6e691d
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-22 10:47:53 +0545

    Update Memory.kt

--- loki/src/commonMain/kotlin/io/github/remmerw/loki/core/Memory.kt
+++ loki/src/commonMain/kotlin/io/github/remmerw/loki/core/Memory.kt
@@ -2,6 +2,20 @@ package io.github.remmerw.loki.core
 
 import java.nio.ByteBuffer
 import java.security.MessageDigest
+import kotlinx.io.Buffer
+import kotlinx.io.RawSink
+import kotlinx.io.RawSource
+import kotlinx.io.Sink
+import kotlinx.io.buffered
+import kotlinx.io.bytestring.getByteString
+import kotlinx.io.files.Path
+import kotlinx.io.files.SystemFileSystem
+import kotlinx.io.readByteArray
+import java.io.RandomAccessFile
+import java.nio.ByteBuffer
+import kotlin.math.min
+import kotlin.uuid.ExperimentalUuidApi
+
 
 internal fun ByteBuffer.toSha1(): ByteArray {
     rewind()
@@ -30,3 +44,31 @@ internal fun ByteBuffer.writeMemory(
         position(offset)
         put(bytes)
     }
+
+    internal fun ByteBuffer.transferTo(sink: RawSink) {
+        rawSource().buffered().transferTo(sink)
+    }
+
+
+        internal fun ByteBuffer.rawSource(): RawSource {
+        rewind()
+
+        return object : RawSource {
+            override fun readAtMostTo(
+                sink: Buffer,
+                byteCount: Long,
+            ): Long {
+                val read = min(byteCount, remaining().toLong())
+                if (read > 0) {
+                    val data = getByteString(read.toInt())
+                    sink.write(data.toByteArray())
+                    return read
+                } else {
+                    return -1
+                }
+            }
+
+            override fun close() {
+                // nothing to do
+            }
+        }
