075d56aba6f8c59894eccd959803c6df32e69299
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-22 10:18:41 +0545

    Aktualisieren von Memory.kt

--- loki/src/commonMain/kotlin/io/github/remmerw/loki/core/Memory.kt
+++ loki/src/commonMain/kotlin/io/github/remmerw/loki/core/Memory.kt
@@ -1,8 +1,29 @@
 import java.nio.ByteBuffer
 import java.security.MessageDigest
 
-fun ByteBuffer.toSha1(): ByteArray {
+internal fun ByteBuffer.toSha1(): ByteArray {
     val md = MessageDigest.getInstance("SHA-1")
     md.update(this)
     return md.digest()
 }
+
+internal fun ByteBuffer.readMemory(
+        offset: Int,
+        length: Int,
+    ): ByteArray {
+        rewind()
+        position(offset)
+        val result = ByteArray(length)
+        get(result)
+        return result
+    }
+
+    
+internal fun ByteBuffer.writeMemory(
+        bytes: ByteArray,
+        offset: Int,
+    ) {
+        rewind()
+        position(offset)
+        put(bytes)
+    }
