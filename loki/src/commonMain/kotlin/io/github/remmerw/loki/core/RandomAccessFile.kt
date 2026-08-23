0abbd504ebe4f7005e03e7229cd66cd5e9964fae
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-23 16:53:43 +0545

    Aktualisieren von RandomAccessFile.kt

--- loki/src/commonMain/kotlin/io/github/remmerw/loki/core/RandomAccessFile.kt
+++ loki/src/commonMain/kotlin/io/github/remmerw/loki/core/RandomAccessFile.kt
@@ -52,49 +52,7 @@ interface RandomAccessFile : AutoCloseable {
     override fun close()
 }
 
-interface Memory {
-    fun writeBytes(
-        bytes: ByteArray,
-        offset: Int,
-    )
 
-    fun size(): Int
-
-    fun readBytes(
-        offset: Int,
-        length: Int,
-    ): ByteArray
-
-    fun transferTo(sink: RawSink) {
-        rawSource().buffered().transferTo(sink)
-    }
-
-    fun rawSource(): RawSource
-}
-
-fun allocateMemory(bytes: ByteArray): Memory {
-    val memory = allocateMemory(bytes.size)
-    memory.writeBytes(bytes, 0)
-    return memory
-}
-
-fun allocateMemory(path: Path): Memory {
-    require(SystemFileSystem.exists(path)) { "path does not exists" }
-    val size = SystemFileSystem.metadataOrNull(path)!!.size.toInt()
-    val memory = allocateMemory(size)
-    val sink = Buffer()
-    var offset = 0
-    SystemFileSystem.source(path).use { source ->
-        do {
-            val read = source.readAtMostTo(sink, UShort.MAX_VALUE.toLong())
-            if (read > 0) {
-                memory.writeBytes(sink.readByteArray(), offset)
-                offset += read.toInt()
-            }
-        } while (read > 0)
-    }
-    return memory
-}
 
 private class RandomAccessFileImpl(
     val raf: RandomAccessFile,
@@ -135,22 +93,6 @@ private class RandomAccessFileImpl(
         raf.write(bytes, offset, length)
     }
 
-    private fun writeMemory(
-        position: Long,
-        memory: Memory,
-    ) {
-        raf.seek(position)
-        val buffer = Buffer()
-        memory.rawSource().use { source ->
-            do {
-                val written = source.readAtMostTo(buffer, SPLITTER)
-                if (written > 0) {
-                    raf.write(buffer.readByteArray())
-                }
-            } while (written > 0)
-        }
-    }
-
     override fun transferTo(
         position: Long,
         sink: Sink,
@@ -178,73 +120,10 @@ private class RandomAccessFileImpl(
     }
 }
 
-internal class MemoryImpl(
-    val memory: ByteBuffer,
-    val size: Int,
-) : Memory {
-    override fun size(): Int = size
 
-    override fun readBytes(
-        offset: Int,
-        length: Int,
-    ): ByteArray {
-        memory.rewind()
-        memory.position(offset)
-        val result = ByteArray(length)
-        memory.get(result)
-        return result
-    }
-
-    override fun writeBytes(
-        bytes: ByteArray,
-        offset: Int,
-    ) {
-        memory.rewind()
-        memory.position(offset)
-        memory.put(bytes)
-    }
-
-    override fun rawSource(): RawSource {
-        memory.rewind()
-
-        return object : RawSource {
-            override fun readAtMostTo(
-                sink: Buffer,
-                byteCount: Long,
-            ): Long {
-                val read = min(byteCount, memory.remaining().toLong())
-                if (read > 0) {
-                    val data = memory.getByteString(read.toInt())
-                    sink.write(data.toByteArray())
-                    return read
-                } else {
-                    return -1
-                }
-            }
-
-            override fun close() {
-                // nothing to do
-            }
-        }
-    }
-}
-
-@OptIn(ExperimentalUuidApi::class)
-fun allocateMemory(size: Int): Memory {
-    val memory = ByteBuffer.allocateDirect(size)
-
-    return MemoryImpl(memory, size)
-}
 
 fun randomAccessFile(path: Path): io.github.remmerw.loki.core.RandomAccessFile {
     val raf = RandomAccessFile(path.toString(), "rw")
     return RandomAccessFileImpl(raf)
 }
 
-internal fun debug(throwable: Throwable) {
-    if (ERROR) {
-        throwable.printStackTrace()
-    }
-}
-
-private const val ERROR: Boolean = true
