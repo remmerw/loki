1266b3c8f327e9964cd63b453d789d50de6f2765
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-23 21:19:54 +0545

    Aktualisieren von TorrentParserTest.kt

--- loki/src/commonTest/kotlin/io/github/remmerw/loki/TorrentParserTest.kt
+++ loki/src/commonTest/kotlin/io/github/remmerw/loki/TorrentParserTest.kt
@@ -95,7 +95,6 @@ class TorrentParserTest {
 
         val bytes = data.readByteArray()
         val buffer = ByteBuffer.wrap(bytes)
-        buffer.flip()
         val reader = BEReader(buffer)
 
         val result = handler.doDecode(peer, reader)
