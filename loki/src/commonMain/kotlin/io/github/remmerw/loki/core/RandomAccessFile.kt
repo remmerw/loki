22bc7e6da9d7b9ff3d93c24ec36bf3ca5cb86216
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-23 17:29:28 +0545

    Aktualisieren von RandomAccessFile.kt

--- loki/src/commonMain/kotlin/io/github/remmerw/loki/core/RandomAccessFile.kt
+++ loki/src/commonMain/kotlin/io/github/remmerw/loki/core/RandomAccessFile.kt
@@ -7,107 +7,62 @@ import kotlin.math.min
 
 const val SPLITTER = 4096L
 
-interface RandomAccessFile : AutoCloseable {
-    fun read(
-        position: Long,
-        bytes: ByteArray,
-    ): Int
 
-    fun writeBoolean(
-        position: Long,
-        boolean: Boolean,
-    )
 
-    fun readBytes(
-        position: Long,
-        bytes: ByteArray,
-        offset: Int = 0,
-        length: Int = bytes.size,
-    )
-
-    fun writeBytes(
-        position: Long,
-        bytes: ByteArray,
-        offset: Int = 0,
-        length: Int = bytes.size,
-    )
-
-    fun transferTo(
-        position: Long,
-        sink: Sink,
-        length: Long,
-    )
-
-    override fun close()
+fun randomAccessFile(path: Path): RandomAccessFile {
+    return RandomAccessFile(path.toString(), "rw")
 }
 
-private class RandomAccessFileImpl(
-    val raf: RandomAccessFile,
-) : io.github.remmerw.loki.core.RandomAccessFile {
-    override fun read(
-        position: Long,
-        bytes: ByteArray,
-    ): Int {
-        raf.seek(position)
-        return raf.read(bytes)
-    }
 
-    override fun writeBoolean(
-        position: Long,
-        boolean: Boolean,
-    ) {
-        raf.seek(position)
-        raf.writeBoolean(boolean)
-    }
 
-    override fun readBytes(
-        position: Long,
-        bytes: ByteArray,
-        offset: Int,
-        length: Int,
-    ) {
-        raf.seek(position)
-        raf.read(bytes, offset, length)
-    }
+fun RandomAccessFile.read(position: Long, bytes: ByteArray): Int {
+    seek(position)
+    return read(bytes)
+}
 
-    override fun writeBytes(
-        position: Long,
-        bytes: ByteArray,
-        offset: Int,
-        length: Int,
-    ) {
-        raf.seek(position)
-        raf.write(bytes, offset, length)
-    }
+fun RandomAccessFile.writeBoolean(position: Long, boolean: Boolean) {
+    seek(position)
+    writeBoolean(boolean)
+}
 
-    override fun transferTo(
-        position: Long,
-        sink: Sink,
-        length: Long,
-    ) {
-        raf.seek(position)
-        val data = ByteArray(SPLITTER.toInt())
-        var stillToRead = length
-        var read: Int
-        var todo: Boolean
-        var min: Int
-        do {
-            read = raf.read(data)
-            todo = read > 0 && stillToRead > 0
-            if (todo) {
-                min = min(read.toLong(), stillToRead).toInt()
-                sink.write(data, 0, min)
-                stillToRead -= read
-            }
-        } while (todo)
-    }
+fun RandomAccessFile.readBytes(
+    position: Long,
+    bytes: ByteArray,
+    offset: Int = 0,
+    length: Int = bytes.size,
+) {
+    seek(position)
+    read(bytes, offset, length)
+}
 
-    override fun close() {
-        raf.close()
+fun RandomAccessFile.writeBytes(
+    position: Long,
+    bytes: ByteArray,
+    offset: Int = 0,
+    length: Int = bytes.size,
+) {
+    seek(position)
+    write(bytes, offset, length)
+}
+
+fun RandomAccessFile.transferToe(
+    position: Long,
+    sink: Sink,
+    length: Long,
+) {
+    seek(position)
+    val data = ByteArray(SPLITTER.toInt())
+    var stillToRead = length
+
+    while (stillToRead > 0) {
+        val toRead = min(SPLITTER, stillToRead).toInt()
+        val read = read(data, 0, toRead)
+        
+        if (read == -1) break
+        
+        sink.write(data, 0, read)
+        stillToRead -= read
     }
 }
 
-fun randomAccessFile(path: Path): io.github.remmerw.loki.core.RandomAccessFile {
-    val raf = RandomAccessFile(path.toString(), "rw")
-    return RandomAccessFileImpl(raf)
-}
+
