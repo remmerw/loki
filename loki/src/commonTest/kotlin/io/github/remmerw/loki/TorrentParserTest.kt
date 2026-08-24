81e8a5581049c28b7517736426f394448e39a6b4
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-24 06:21:19 +0545

    Aktualisieren von TorrentParserTest.kt

--- loki/src/commonTest/kotlin/io/github/remmerw/loki/TorrentParserTest.kt
+++ loki/src/commonTest/kotlin/io/github/remmerw/loki/TorrentParserTest.kt
@@ -34,7 +34,7 @@ class TorrentParserTest {
     fun parseTorrent(): Unit =
         runBlocking(Dispatchers.IO) {
 
-            val content = "d8:announce19:http://localhost.com4:infod6:lengthi0e4:name10:testfile.tx12:piece lengthi262144e6:pieces20:00000000000000000000ee"
+            val content = "d8:announce16:http://localhost4:infod5:filesld6:lengthi5e4:pathl9:datei1.txteed6:lengthi10e4:pathl9:datei2.dateee4:name11:mein_ordner12:piece lengthi16384e6:pieces20:00000000000000000000ee";
             
 
             val path = Path(SystemTemporaryDirectory, Uuid.random().toHexString())
