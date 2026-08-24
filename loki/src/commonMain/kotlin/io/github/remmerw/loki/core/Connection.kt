efb92a2f7153cb11462312376838d98b0b19292a
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-24 21:11:11 +0545

    Aktualisieren von Connection.kt

--- loki/src/commonMain/kotlin/io/github/remmerw/loki/core/Connection.kt
+++ loki/src/commonMain/kotlin/io/github/remmerw/loki/core/Connection.kt
@@ -64,8 +64,8 @@ internal class Connection internal constructor(
     private val receiveChannel = Channels.newChannel(socket.inputStream)
     private val sendChannel = Channels.newChannel(socket.outputStream)
     private var writeBlock: PooledByteArray? = null
-    private sending = ByteBuffer.allocate(10024)// todo
-    private reading = ByteBuffer.allocate(10024)// todo
+    private val sending = ByteBuffer.allocate(10024)// todo
+    private val reading = ByteBuffer.allocate(10024)// todo
     fun writeBlock(): ByteArray {
         if (writeBlock == null) {
             writeBlock = ByteArrayPool.getInstance(BLOCK_SIZE).get()
