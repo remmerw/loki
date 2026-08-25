23b392e83eabfc79497d735fc13c931975e4f597
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-25 16:50:14 +0545

    Block.kt erstellen

---
+++ loki/src/commonMain/kotlin/io/github/remmerw/loki/core/Block.kt
@@ -0,0 +1,41 @@
+package io.github.remmerw.loki.core
+
+import io.github.remmerw.loki.BLOCK_SIZE
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
+
+internal class Block(
+    private val pool: BlockPool,
+) : AutoCloseable {
+    val data: ByteArray = ByteArray(BLOCK_SIZE)
+
+    override fun close() {
+        pool.release(this)
+    }
+}
+
+internal class BlockPool private constructor() {
+    private val lock = ReentrantLock()
+
+    private val free = ArrayDeque<Block>()
+
+    internal fun release(item: Block) {
+        lock.withLock {
+            free.addLast(item)
+        }
+    }
+
+    fun get(): Block {
+        lock.withLock {
+            return free.removeLastOrNull() ?: Block(this)
+        }
+    }
+
+    companion object {
+        val instance: BlockPool by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
+            BlockPool()
+        }
+    }
+}
+
+internal fun blockInstance(): Block = BlockPool.instance.get()
